9bb88a86584e4fcc81a5ca263ee72132c436d5a4
Katherine Wu <kwu@newrelic.com>
2014-12-10 14:41:27 -0800

    Add explanatory comments

--- solutions/kwugirl/solution.rb
+++ solutions/kwugirl/solution.rb
@@ -1,21 +1,21 @@
 # COUNT_CALLS_TO='String#size' ruby -r ./solution.rb -e '(1..100).each{|i| i.to_s.size if i.odd? }'
 # String#size called 50 times
 
-# require 'solution.rb'
-# (1..100).each{|i| i.to_s.size if i.odd? }
-
 COUNTS = Hash.new{|h,k| h[k] = 0}
 
 class MethodSignature
   attr_accessor :klass, :klass_name, :scope, :method, :to_s
 
   def initialize(string)
+    # to be able to reference the original method name as string in output
     @to_s = string.to_s
+
     @klass_name, @scope, @method = string.to_s.split(/([#.])/)
   end
 
+  # from a string, goes down from Object to find the actual class
+  # so it's the class object, not a string
   def klass
-    # from a string, goes down from Object to find the actual class
     @klass ||= begin
       @klass_name.split('::').inject(Object){|n,i| n.const_get(i)}
     rescue NameError => e
@@ -29,6 +29,8 @@ end
 
 method = signature.method
 
+# make the method redefinition a string in order to be able to
+# do string interpolation
 signature.klass.class_eval <<-RB
   def #{method}_with_counting
     COUNTS[#{method}] += 1
